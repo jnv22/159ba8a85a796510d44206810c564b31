37470bd300820fbe777d112535a6946af14eb82d
Jordan Vartanian <jnv22@cornell.edu>
2018-01-27 17:26:25 -0600

    setup environment run basic server setup

---
+++ app/index.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const express = require('express');
+
+const app = express();
+
+
+app.listen(process.env.PORT);
+console.log(`Listening on port ${process.env.PORT}`);
